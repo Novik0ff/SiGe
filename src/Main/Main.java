ad3571f9bb0a5b681396248c88693c72e166d47f
Novik0ff <a-novik0ff@mail.ru>
2019-02-10 17:13:06 +0300

    добавлен пакет с расчетом энергии

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,7 @@
 package Main;
 
 
+import Energy.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,32 +9,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import Struct.*;
+import Energy.*;
 
 import java.util.Random;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("SiGe");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
         Struct struct = new Struct();
-        if(struct.сreateCubicStructure(20,0.2)){
+        if (struct.сreateCubicStructure(5, 0.2)) {
             System.out.println("Структура успешно создана");
-            if(struct.setAtomsTypes(0.5,0)){
+            if (struct.setAtomsTypes(0.5, 0)) {
                 System.out.println("Типы атомов в структуре заданы");
             }
         }
 
-        int temp = Math.abs(new Random().nextInt())%struct.getStructCountAtoms();
+        int temp = Math.abs(new Random().nextInt()) % struct.getStructCountAtoms();
         System.out.println(struct.getAtom(temp).getAtomInfo());
-        temp = Math.abs(new Random().nextInt())%struct.getStructCountAtoms();
+        temp = Math.abs(new Random().nextInt()) % struct.getStructCountAtoms();
         System.out.println(struct.getAtom(temp).getAtomInfo());
         System.out.println(struct.getStructCountAtoms());
-        int t=0;
+        Constants.setTersoff();
+        System.out.println(Constants.tersoffs[0].A);
+        int t = 0;
 
     }
 
